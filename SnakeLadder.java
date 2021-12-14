ae1ff8e3163b7d0ce9380bb8f8f165d08b05a8fb
unknown <>
2021-12-14 20:50:26 +0530

    ADDED: player roll the die to get a number between one to six

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,4 +4,12 @@ public class Snake{
 	}
 	public static void start() {
 		int position =1;  //initail position is 1 because game starts from 1 on board
+		int dice=roll();
+		System.out.println("Your number is "+dice);
 	}
+	public static int roll() {
+
+		return (int)Math.floor(Math.random()*(6-1+1)+1);
+
+	}
+}
