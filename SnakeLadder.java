bad03f163f9c4ff7725f534c740c49b03d7d7d92
unknown <>
2021-12-14 21:07:07 +0530

    ADDED: report the number of times dice was played to win game

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,16 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 public class Snake{
 	public static void main (String[] args) {
 		start();
 	}
 	public static void start() {
+		Map<Integer, Integer> report=new HashMap<>(); //to store position after every dice roll
+		int counter=0; //to count the no of dice roll
 		Scanner sc=new Scanner(System.in);
 		int position =1;  //initail position is 1 because game starts from 1 on board
                 int prev_position;
-     		while(position!=100) {
+		while(position!=100) {
                         System.out.println("Your position is "+position);
 			prev_position=position;
-     			int dice=roll();
+			int dice=roll();
 			System.out.println("Your number is "+dice);
 			System.out.println("choose option");
 			String option = sc.next();
@@ -44,6 +48,8 @@ public class Snake{
 
 			}
 		}
+		System.out.println("you have won");
+		System.out.println(report); //to return the no of dice was played to win the game and position after every dice roll
 	}
 	public static int roll() {
 
