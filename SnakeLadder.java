138196d8165364f5b45a64e9a658b4570fb2ecb3
unknown <>
2021-12-14 20:44:52 +0530

    ADDED: single player at start position zero

---
+++ SnakeLadder.java
@@ -0,0 +1,7 @@
+public class Snake{
+	public static void main (String[] args) {
+		start();
+	}
+	public static void start() {
+		int position =1;  //initail position is 1 because game starts from 1 on board
+	}
